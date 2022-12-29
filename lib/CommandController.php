4d5d6484261fea9b8a41846f2c68fe75c5d4083b
Egor <glavnegor@yandex.ru>
2022-12-29 21:12:18 +0300

    fix class CommandController

--- lib/CommandController.php
+++ lib/CommandController.php
@@ -5,11 +5,14 @@ namespace CliClack\Lib;
 abstract class CommandController
 {
     protected App $app;
+
     public function __construct(App $app)
     {
         $this->app = $app;
     }
+
     abstract public function run($argv);
+
     public function getApp(): App
     {
         return $this->app;
